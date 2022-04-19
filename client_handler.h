f1b0eebab1376ad14b2842af9e6fa825268035a9
daxianhorse <1359672919@qq.com>
2022-04-19 18:15:51 +0800

    Init Client Handler

---
+++ client_handler.h
@@ -0,0 +1,12 @@
+//
+// Created by liang on 22-4-19.
+//
+
+#ifndef QBROWSERCLIENT__CLIENT_HANDLER_H_
+#define QBROWSERCLIENT__CLIENT_HANDLER_H_
+
+class client_handler {
+
+};
+
+#endif //QBROWSERCLIENT__CLIENT_HANDLER_H_
